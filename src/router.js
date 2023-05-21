f370074d4a0036231cd2f60af9da68fcfa61186a
Doter123 <nogamenolive892@gmail.com>
2023-05-21 15:31:23 +0600

    first commit

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    { path: '/', component: () => import('./components/Home.vue') },
+    { path: '/room', component: () => import('./components/HomeRoom.vue') },
+    { path: '/profile', component: () => import('./components/HomeProfile.vue') },
+    { path: '/admin', component: () => import('./components/Admin.vue') },
+    { path: '/bookings', component: () => import('./components/Bookings.vue') },
+    { path: '/login', component: () => import('./components/Login.vue') },
+    { name: 'register', path: '/register', component: () => import('./components/Register.vue') }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
